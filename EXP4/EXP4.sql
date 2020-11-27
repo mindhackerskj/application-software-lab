cb0987bb8ea389d473300d2c8f1646cce7ab4ba0
Saurav Kj <sauravkjtply@gmail.com>
2020-11-27 12:52:56 +0530

    Completed

---
+++ EXP4/EXP4.sql
@@ -0,0 +1,24 @@
+#a
+
+CREATE TABLE Department(
+    `code` INT PRIMARY KEY NOT NULL,
+    `title` VARCHAR(30),
+    `dept_name` VARCHAR(30) UNIQUE NOT NULL,
+    `dept_id` INT UNIQUE NOT NULL,
+    `salary` INT,
+    CHECK (`salary` > 2000 )
+);
+
+INSERT INTO Department(`code`, `title`, `dept_name`, `dept_id`,`salary`) 
+VALUES (221, "Test Title", "Computer Science", 100, 20000);
+
+#b
+
+CREATE TABLE Instructor(
+    `name` VARCHAR(50) NOT NULL,
+    `code` INT NOT NULL,
+    `id` INT PRIMARY KEY DEFAULT 0
+);
+
+INSERT INTO Instructor(`name`, `code`)
+VALUES ("Peter", 369);
